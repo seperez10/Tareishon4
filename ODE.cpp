c1d1954e5ff3757dc015b1b69fa38db0a567359e
seperez10 <se.perez10@uniandes.edu.co>
2018-11-19 17:44:02 -0500

    creo que termine

--- ODE.cpp
+++ ODE.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 using std::cout;
 using std::cin;
-
+ofstream datossalida("datosproyect.txt");
+ofstream datosadic("adicional.txt");
 //Defino las variables que me brinda el enunciado de la tarea y hacia donde redireccionare los archivos
 double c=0.2;
 double m=0.2;
@@ -15,8 +16,7 @@ double velo=300.0;
 double dx;
 double dy;
 double pi=3.1416;
-ofstream datossalida("datosproyect.txt");
-ofstream datosadic("adicional.txt");
+
 
 
 //Es necesario crear una funcion para encontrar cual sera la derivada de la velocidad en x pero para eso necesito tener la derivada tanto en y como en x de la posicion 
@@ -32,18 +32,17 @@ double derix( double dx)
 //////En el paso anterior estan las funciones para que se puedan utilizar con facilidad las derivadas de las posiciones
 //////Primero es necesario hallar la magnitud de la velocidad simplemente con la raiz de la suma de las dos componentes elevadas al cuadrado cada una
 
-double magnitudvelo= sqrt(pow(dx,2)+pow(dy,2));
 
-double deriveloy(double dy,double dx){
 
+double deriveloy(double dx,double dy){
+ double magnitudvelo= sqrt((pow(dx,2)+pow(dy,2)));
  resultini= -c*((pow(magnitudvelo,2)/m)*(dy/magnitudvelo));
+ 
  return resultini;
 }
-
-double derivelox(double dy,double dx){
-
+double derivelox(double dx,double dy){
+double magnitudvelo= sqrt((pow(dx,2)+pow(dy,2)));
  resultini= -c*((pow(magnitudvelo,2)/m)*(dx/magnitudvelo));
-
  return resultini;
 }
 
@@ -51,8 +50,9 @@ double derivelox(double dy,double dx){
 
 double calculitos (double angulito){
 //En primer lugar para que las cosas funcionen es necesario pasar el angulo de radianes a grados y definimos cual quiero que sea mi modificacion de tiempo que en este caso sera 0.0001 ya que no se brinda una en el enunciado
- double dt=0.0001;
- double angulitogr= ((angulito*pi)/180.0);
+ double angulitogr= (angulito/180.0*3.1416);
+ double dt=0.0002;
+ cout<<"resolvemos la trayectoria del proyectil cuando es lanzado a" << angulito<<"°"<<endl;
 //Necesitamos tener los datos del inicio y tambien de alguna condicion futura que pueda existir
  double xf;
  double yf;
@@ -88,27 +88,27 @@ double calculitos (double angulito){
  double var4dyf=0.0;
 
 /////Aca realizaremos la derivada para el unico punto que conocemos en los 4 casos 
- int i=0;
+int i=0;
  do{
-
+int i=0;
   var1xf= derix(dxini);
   var1yf= derix(dyini);
   var1dxf= derivelox(dyini,dxini);
-  var1dxf= deriveloy(dyini,dxini);
+  var1dyf= deriveloy(dyini,dxini);
 
 //Posteriormente con forward y las diferentes variables creadas anteriormente se intentara calcular la derivada adelante utilizando la que calculamos anteriormente y asi consecutivamente hasta tener todas las variables para asi lograr hallar la pendiente de cada uno de los casos tomando los valores de las variables en x en y en dx o dy
   var2xf= derix(dxini+0.5*dt*var1dxf); 
   var2yf= deriy(dyini+0.5*dt*var1dyf);
   var2dxf= derivelox(dxini+0.5*dt*var1dxf, dyini+0.5*dt*var1dyf);
-  var2dxf= deriveloy(dxini+0.5*dt*var1dxf, dyini+0.5*dt*var1dyf);
+  var2dyf= deriveloy(dxini+0.5*dt*var1dxf, dyini+0.5*dt*var1dyf);
   var3xf= derix(dxini+0.5*dt*var2dxf); 
   var3yf= deriy(dyini+0.5*dt*var2dyf);
   var3dxf= derivelox(dxini+0.5*dt*var2dxf, dyini+0.5*dt*var2dyf);
-  var3dxf= deriveloy(dxini+0.5*dt*var2dxf, dyini+0.5*dt*var2dyf); 
-  var4xf= derix(dxini+0.5*dt*var3dxf); 
-  var4yf= deriy(dyini+0.5*dt*var3dyf);
-  var4dxf= derivelox(dxini+0.5*dt*var3dxf, dyini+0.5*dt*var3dyf);
-  var4dxf= deriveloy(dxini+0.5*dt*var3dxf, dyini+0.5*dt*var3dyf);
+  var3dyf= deriveloy(dxini+0.5*dt*var2dxf, dyini+0.5*dt*var2dyf); 
+  var4xf= derix(dxini+1*dt*var3dxf); 
+  var4yf= deriy(dyini+1*dt*var3dyf);
+  var4dxf= derivelox(dxini+1*dt*var3dxf, dyini+1*dt*var3dyf);
+  var4dyf= deriveloy(dxini+1*dt*var3dxf, dyini+1*dt*var3dyf);
   pendxf=(1.0*var1xf+2.0*var2xf+2.0*var3xf+1.0*var4xf)/6.0;  
   pendyf=(1.0*var1yf+2.0*var2yf+2.0*var3yf+1.0*var4yf)/6.0;
   penddxf=(1.0*var1dxf+2.0*var2dxf+2.0*var3dxf+1.0*var4dxf)/6.0;
@@ -127,18 +127,33 @@ double calculitos (double angulito){
   yini=yf;
   dxini=dxf;
   dyini=dyf;
-  i=i+1.0;
-} while(i!=0 && yini>0);
+  i=i+1;
+} while(i=0 && yini>0);
 //Despues de esto ya sabremos cual es el xf el cual representara nuestra distancia recorrida total horizontalmente
- cout<<"La distancia total recorrida horizontalmente por el proyectil es"<<xini<<endl;
  datosadic<<i<<endl;
- cout<<"La trayectoria que se calcula para el proyectil a"<<angulito<<"rad"<<endl<<endl;
-datossalida.close();
-datosadic.close();
+ cout<<"La distancia total recorrida horizontalmente por el proyectil es" << xini <<endl;
+ cout<<"La trayectoria que se calcula para el proyectil a" << angulito << " ° "<<endl<<endl;
+//datossalida.close();
+//datosadic.close();
  return xini;
 }
-
-int main(int nc, char const *argumv[]){
-	
+//Aqui realizaremos lo que tiene que mostrar o sea lo mas importante e inicializamos las variables de nuevo
+int main(int argc, char const *argv[]){
+ double angulito=45.0;
+ double xini= calculitos(angulito);
+ double distance[7];
+ ///Ahora es necesario encontrar el angulo al cual presentara una mayor distancia el proyectil como lo dice el enunciado
+for (int i=0;i<7;++i){
+ angulito= 10.0*(i+1);
+ distance[i]=calculitos(angulito);
+}
+int maximo=6;
+for (int i=0;i<7;i++){
+ if (distance[maximo]<distance[i]){
+  maximo=i;
+ }
+}
+cout<<"Donde se presenta una mayor distancia es cuando el proyectil es lanzado a un angulo de" << (maximo+1)*10 <<endl<<endl;	
+ return 0;
 }
 
