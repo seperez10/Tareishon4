45cbfc72905375250e628b43488b1b706020fcbd
seperez10 <se.perez10@uniandes.edu.co>
2018-11-19 09:56:26 -0500

    Defino las variables dadas en el enunciado

--- ODE.cpp
+++ ODE.cpp
@@ -4,7 +4,14 @@
 #include<fstream>
 using std::cout;
 using std::cin;
-
-
+//Defino las variables que me brinda el enunciado de la tarea 
+double c;
+c=0.2;
+double m;
+m=0.2;
+double resultini;
+resultini=0.0;
+double g;
+g=10.0;
 int main(){
 }
