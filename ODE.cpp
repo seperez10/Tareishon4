5dcec5bb58f90d34849b8d27788a74f2695a0fe8
seperez10 <se.perez10@uniandes.edu.co>
2018-11-19 10:36:30 -0500

    Realizo dos funciones para devolver las posiciones

--- ODE.cpp
+++ ODE.cpp
@@ -5,13 +5,30 @@
 using std::cout;
 using std::cin;
 //Defino las variables que me brinda el enunciado de la tarea 
-double c;
-c=0.2;
-double m;
-m=0.2;
-double resultini;
-resultini=0.0;
-double g;
-g=10.0;
+double c=0.2;
+double m=0.2;
+double resultini=0.0;
+double g=10.0;
+double velo=300.0;
+double dx;
+double dy;
+
+//Es necesario crear una funcion para encontrar cual sera la derivada de la velocidad en x pero para eso necesito tener la derivada tanto en y como en x de la posicion 
+double deriy( double dy)
+{
+ return dy;
+}
+double derix( double dx)
+{
+ return dx;
+}
+//En el paso anterior estan las funciones para que se puedan utilizar con facilidad las derivadas de las posiciones
+double deriveloy(double dy,double dx)
+{
+ double magnitudvelo;
+
+}
+
 int main(){
 }
+
